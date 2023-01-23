9a546e9f1bfca097e8d31c02b72439de0fc6c8fd
André <andre.kreutzer@outlook.com>
2023-01-22 22:45:57 -0300

    Implementing Add button action and local storage

--- scrips.js
+++ scrips.js
@@ -1,11 +1,32 @@
 const form = document.querySelector("#form-habits")
 const nlwSetup = new NLWSetup(form)
 
-const data = {
-  run: ["01-01", "01-02", "01-06", "01-07", "01-08"],
-  takePills: ["01-03"],
-  journal: ["01-02"],
+//creating dynamic add button
+const button = document.querySelector("header button")
+
+button.addEventListener("click", add)
+form.addEventListener("change", save)
+
+function add() {
+  //adding alert for already existing days
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
+  const dayExists = nlwSetup.dayExists(today)
+
+  if (dayExists) {
+    alert("Day already registered 🔴")
+    return
+  }
+
+  alert("Day registered successfully ✅")
+  nlwSetup.addDay(today)
 }
 
+function save() {
+  // localStorage value cannot be an object so we're using JSON.stringify to transform it into a string
+  localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data))
+}
+
+// now the data cannot be display as string and we should return it back as object using JSON.parse
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
 nlwSetup.setData(data)
 nlwSetup.load()
